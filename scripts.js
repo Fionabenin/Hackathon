16515abfdf55a52c2f68c3eaffe02afd4239374e
Fionabenin <155658848+Fionabenin@users.noreply.github.com>
2024-01-06 09:29:58 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Website</title>
+</head>
+
+<body>
+
+    <script>
+        // JavaScript code
+        window.onload = function () {
+            alert('Welcome to My Personal Website!');
+        };
+    </script>
+
+</body>
+
+</html>
